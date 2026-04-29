c3ac4cd73fc8b36c498139c6fc1ea9436dfc0642
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2026-04-28 21:40:47 -0300

    inicia projeto

---
+++ src/main/java/br/ufjf/sgcvapi/SgcvapiApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.sgcvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcvapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcvapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/sgcvapi/SgcvapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.sgcvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcvapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
