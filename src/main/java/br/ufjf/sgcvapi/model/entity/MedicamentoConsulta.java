07ddbef823193330f66eea9747f6dd84b5ca3679
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2026-04-30 22:20:35 -0300

    Adiciona Medicamento e suas classes auxiliares

---
+++ src/main/java/br/ufjf/sgcvapi/model/entity/MedicamentoConsulta.java
@@ -0,0 +1,23 @@
+package br.ufjf.sgcvapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MedicamentoConsulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Medicamento medicamento;
+
+    @ManyToOne
+    private Consulta consulta;
+}
---
+++ src/main/java/br/ufjf/sgcvapi/model/entity/MedicamentoPet.java
@@ -0,0 +1,23 @@
+package br.ufjf.sgcvapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MedicamentoPet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Medicamento alergia;
+
+    @ManyToOne
+    private Pet pet;
+}
