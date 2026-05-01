625f178c1b16b0fab70bb3e18e5816989786ec4a
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-04-30 22:23:09 -0300

    adiciona geração de id à classe vacina

--- src/main/java/br/ufjf/sgcvapi/model/entity/Vacina.java
+++ src/main/java/br/ufjf/sgcvapi/model/entity/Vacina.java
@@ -1,6 +1,8 @@
 package br.ufjf.sgcvapi.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,8 +15,10 @@ import lombok.NoArgsConstructor;
 public class Vacina{
 
     @Id
-    private String numLote;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String numLote;
     private String nome;
     private String descricao;
     private Float valor;
