39be5fbd132f42f3a967c205e620ceac0e0a4588
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2026-06-02 19:50:16 -0300

    cria Services para ServicoEspecializacao e VacinaConsulta]

---
+++ src/main/java/br/ufjf/sgcvapi/service/ServicoEspecializacaoService.java
@@ -0,0 +1,48 @@
+package br.ufjf.sgcvapi.service;
+
+import br.ufjf.sgcvapi.exception.RegraNegocioException;
+import br.ufjf.sgcvapi.model.entity.ServicoEspecializacao;
+import br.ufjf.sgcvapi.model.repository.ServicoEspecializacaoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ServicoEspecializacaoService {
+    private ServicoEspecializacaoRepository repository;
+
+    public ServicoEspecializacaoService(ServicoEspecializacaoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<ServicoEspecializacao> getServicoEspecializacaos() {
+        return repository.findAll();
+    }
+
+    public Optional<ServicoEspecializacao> getServicoEspecializacaoById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public ServicoEspecializacao salvar(ServicoEspecializacao servicoEspecializacao) {
+        validar(servicoEspecializacao);
+        return repository.save(servicoEspecializacao);
+    }
+
+    @Transactional
+    public void excluir(ServicoEspecializacao servicoEspecializacao) {
+        Objects.requireNonNull(servicoEspecializacao.getId());
+        repository.delete(servicoEspecializacao);
+    }
+
+    public void validar(ServicoEspecializacao servicoEspecializacao) {
+        if (servicoEspecializacao.getServico() == null || servicoEspecializacao.getServico().getId() == null || servicoEspecializacao.getServico().getId() == 0) {
+            throw new RegraNegocioException("Serviço inválido");
+        }
+        if (servicoEspecializacao.getEspecializacao() == null || servicoEspecializacao.getEspecializacao().getId() == null || servicoEspecializacao.getEspecializacao().getId() == 0) {
+            throw new RegraNegocioException("Especialização inválida");
+        }
+    }
+}
