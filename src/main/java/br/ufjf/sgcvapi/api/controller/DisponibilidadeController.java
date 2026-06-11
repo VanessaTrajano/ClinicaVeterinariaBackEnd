aff604fc5c01eeddf3859aa6028ed0d5245e0d47
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2026-06-11 20:09:33 -0300

    Cria controllers de Consulta e Disponibilidade

---
+++ src/main/java/br/ufjf/sgcvapi/api/controller/DisponibilidadeController.java
@@ -0,0 +1,93 @@
+package br.ufjf.sgcvapi.api.controller;
+
+import br.ufjf.sgcvapi.api.dto.DisponibilidadeDTO;
+import br.ufjf.sgcvapi.exception.RegraNegocioException;
+import br.ufjf.sgcvapi.model.entity.Disponibilidade;
+import br.ufjf.sgcvapi.model.entity.Veterinario;
+import br.ufjf.sgcvapi.service.DisponibilidadeService;
+import br.ufjf.sgcvapi.service.VeterinarioService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/disponibilidades")
+@RequiredArgsConstructor
+@CrossOrigin
+public class DisponibilidadeController {
+
+    private final DisponibilidadeService service;
+    private final VeterinarioService veterinarioService;
+
+    @GetMapping()
+    public ResponseEntity get() {
+        List<Disponibilidade> disponibilidades = service.getDisponibilidades();
+        return ResponseEntity.ok(disponibilidades.stream().map(DisponibilidadeDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id) {
+        Optional<Disponibilidade> disponibilidade = service.getDisponibilidadeById(id);
+        if (!disponibilidade.isPresent()) {
+            return new ResponseEntity("Disponibilidade não encontrada", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(disponibilidade.map(DisponibilidadeDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(@RequestBody DisponibilidadeDTO dto) {
+        try {
+            Disponibilidade disponibilidade = converter(dto);
+            disponibilidade = service.salvar(disponibilidade);
+            return new ResponseEntity(disponibilidade, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody DisponibilidadeDTO dto) {
+        if (!service.getDisponibilidadeById(id).isPresent()) {
+            return new ResponseEntity("Disponibilidade não encontrada", HttpStatus.NOT_FOUND);
+        }
+        try {
+            Disponibilidade disponibilidade = converter(dto);
+            disponibilidade.setId(id);
+            service.salvar(disponibilidade);
+            return ResponseEntity.ok(disponibilidade);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity excluir(@PathVariable("id") Long id) {
+        Optional<Disponibilidade> disponibilidade = service.getDisponibilidadeById(id);
+        if (!disponibilidade.isPresent()) {
+            return new ResponseEntity("Disponibilidade não encontrada", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(disponibilidade.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public Disponibilidade converter(DisponibilidadeDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Disponibilidade disponibilidade = modelMapper.map(dto, Disponibilidade.class);
+        Optional<Veterinario> veterinario = veterinarioService.getVeterinarioById(dto.getIdVeterinario());
+        if(!veterinario.isPresent()) {
+            throw new RegraNegocioException("Veterinário não encontrada");
+        }
+        cliente.setVeterinario(veterinario.get());
+        return disponibilidade;
+    }
+}
