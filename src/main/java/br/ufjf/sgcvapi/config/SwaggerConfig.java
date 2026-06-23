32cfc847f483616952d4101e2ecc9a4e66dbf075
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-06-23 08:03:06 -0300

    Adiciona configuração do swagger

---
+++ src/main/java/br/ufjf/sgcvapi/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package br.ufjf.sgcvapi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SGCV API")
+                        .description("Sistema de Gestão de Clínica Veterinária")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Vanessa P K Trajano & Eduarda P M Nunes")
+                                .url("https://github.com/VanessaTrajano/ClinicaVeterinariaBackEnd")
+                                .email("vanessa.trajano@estudante.ufjf.br, eduarda.nunes@estudante.ufjf.br")
+                        )
+                )
+
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
+    }
+}
