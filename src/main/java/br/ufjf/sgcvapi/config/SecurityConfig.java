36ab6d9247aedb84cfbf77c10e761c87cfad2121
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-06-23 18:53:36 -0300

    Tenta atualizar o securityConfig para a nova versão do spring boot

--- src/main/java/br/ufjf/sgcvapi/config/SecurityConfig.java
+++ src/main/java/br/ufjf/sgcvapi/config/SecurityConfig.java
@@ -6,21 +6,26 @@ import br.ufjf.sgcvapi.service.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.context.annotation.Configuration;
+
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
 
     @Autowired
     private UsuarioService usuarioService;
@@ -38,78 +43,93 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new JwtAuthFilter(jwtService, usuarioService);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .userDetailsService(usuarioService)
-            .passwordEncoder(passwordEncoder());
-    }
+    // ============================== LEGADO ============================== //
+    //    @Bean
+    //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //        auth
+    //            .userDetailsService(usuarioService)
+    //            .passwordEncoder(passwordEncoder());
+    //    }
+    // ===================================================================== //
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .cors().disable()
-            .csrf().disable()
-            .authorizeRequests()
-                .antMatchers("/api/v1/agendamentos/**")
-                    .permitAll()
-                .antMatchers("/api/v1/clientes/**")
-                    .permitAll()
-                .antMatchers("/api/v1/consultas/**")
-                    .permitAll()
-                .antMatchers("/api/v1/consultaServicos/**")
-                    .permitAll()
-                .antMatchers("/api/v1/consultaVeterinarios/**")
-                    .permitAll()
-                .antMatchers("/api/v1/disponibilidades/**")
-                    .permitAll()
-                .antMatchers("/api/v1/enderecos/**")
-                    .permitAll()
-                .antMatchers("/api/v1/especializacoes/**")
-                    .permitAll()
-                .antMatchers("/api/v1/especies/**")
-                    .permitAll()
-                .antMatchers("/api/v1/medicamentoConsultas/**")
-                    .permitAll()
-                .antMatchers("/api/v1/medicamentos/**")
-                    .permitAll()
-                .antMatchers("/api/v1/medicamentoPets/**")
-                    .permitAll()
-                .antMatchers("/api/v1/pets/**")
-                    .permitAll()
-                .antMatchers("/api/v1/racas/**")
-                    .permitAll()
-                .antMatchers("/api/v1/servicos/**")
-                    .permitAll()
-                .antMatchers("/api/v1/servicoEspecializacaos/**")
-                    .permitAll()
-                .antMatchers("/api/v1/vacinaConsultas/**")
-                    .permitAll()
-                .antMatchers("/api/v1/vacinas/**")
-                    .permitAll()
-                .antMatchers("/api/v1/veterinarios/**")
-                    .permitAll()
-                .antMatchers("/api/v1/veterinarioEspecializacoes/**")
-                    .permitAll()
-//                .antMatchers("/api/v1/vagas/**")
-//                    .hasAnyRole("USER", "ADMIN")
-                .anyRequest().authenticated()
-            .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
-        ;
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(
-                "/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html",
-                "/webjars/**");
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> cors.disable())
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/agendamentos/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/clientes/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/consultas/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/consultaServicos/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/consultaVeterinarios/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/disponibilidades/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/enderecos/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/especializacoes/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/especies/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/medicamentoConsultas/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/medicamentos/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/medicamentoPets/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/pets/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/racas/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/servicos/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/servicoEspecializacaos/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/vacinaConsultas/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/vacinas/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/veterinarios/**")
+                    .permitAll()
+                .requestMatchers("/api/v1/veterinarioEspecializacoes/**")
+                    .permitAll()
+
+                // configure(WebSecurity)
+                .requestMatchers("/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/webjars/**"
+                )
+                    .permitAll()
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+        ;
     }
+
+    // ============================== LEGADO ============================== //
+    //    @Override
+    //    public void configure(WebSecurity web) throws Exception {
+    //        web.ignoring().antMatchers(
+    //                "/v2/api-docs",
+    //                "/configuration/ui",
+    //                "/swagger-resources/**",
+    //                "/configuration/security",
+    //                "/swagger-ui.html",
+    //                "/webjars/**");
+    //    }
+    // ===================================================================== //
 }
